dcd5be09f4817e985bc2dd5ab4b1ad0aa3db8155
wellflag <itcwangfeng@hotmail.com>
2021-06-06 20:21:15 -0500

    Updates

---
+++ precache-manifest.1a7e4066db23af8ceb86992e8500e9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e4d31f3df52e3f3d883bc7c617032a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221ed3c36992b77546f5",
+    "url": "/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "8b1306c0cbbddee2e9fe",
+    "url": "/static/js/2.bec93215.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bec93215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221ed3c36992b77546f5",
+    "url": "/static/js/main.063e4ceb.chunk.js"
+  },
+  {
+    "revision": "a62cc204fd5327a8eb1d",
+    "url": "/static/js/runtime-main.4bbc5e43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react01/precache-manifest.8a4a2abc9df595c49eb7ea91abf69c62.js"
+  "/precache-manifest.1a7e4066db23af8ceb86992e8500e9d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react01/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
